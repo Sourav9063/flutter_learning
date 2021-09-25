138ede5e0208b62a8e70ab2ecda5e9052a327309
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2021-09-25 19:26:13 +0600

    TapToShrink button

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/Screens/TapShrinkButton/TapShrinkButton.dart';
 import 'package:flutter_learning/Screens/TweenCounter/TweenCounter.dart';
 
 import 'Screens/BounceAnimation/BounceNavScreen.dart';
@@ -12,8 +13,11 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          SizedBox(
+            width: double.infinity,
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ElevatedButton(
@@ -38,6 +42,30 @@ class HomePage extends StatelessWidget {
                     ));
               },
               child: const Text("TweenAnimation"),
+            ),
+          ),
+          TapShrinkButton(
+            shrinkfactor: .8,
+            onTap: () {
+              print("tap");
+              Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (context) => BounceAnimatedNavBar(),
+                  ));
+            },
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              // width: MediaQuery.of(context).size.width * .5,
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 6),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
+              ),
+              child: const Text(
+                "TapShrinkButton",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           )
         ],
